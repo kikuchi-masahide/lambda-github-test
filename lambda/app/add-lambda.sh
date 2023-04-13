cf9c24cc31a2e7d1e5fc6c0d1dd4e28e1f54a061
菊池雅英 <kikuchi.masahide.86n@st.kyoto-u.ac.jp>
2023-04-13 23:33:19 +0900

    aws samを用いたdeploy

--- lambda/app/add-lambda.sh
+++ lambda/app/add-lambda.sh
@@ -4,10 +4,7 @@ echo "lambda名: "
 read name
 
 # github-actionテンプレートのコピー
-echo "../workflows/$name.yml"
 cp base.yml "../workflows/$name.yml"
-echo "/workflows/$name.yml"
-ls ../workflows
 sed -i "s/FUNCTION_NAME/"$name"/g" "../workflows/$name.yml"
 
 mkdir $name
