2e973c94a0aa6bef7e939fc1f482395ffdc9a159
菊池雅英 <kikuchi.masahide.86n@st.kyoto-u.ac.jp>
2023-04-13 23:18:30 +0900

    lambdaを追加できるよう編集

--- lambda/app/add-lambda.sh
+++ lambda/app/add-lambda.sh
@@ -3,6 +3,13 @@
 echo "lambda名: "
 read name
 
+# github-actionテンプレートのコピー
+echo "../workflows/$name.yml"
+cp base.yml "../workflows/$name.yml"
+echo "/workflows/$name.yml"
+ls ../workflows
+sed -i "s/FUNCTION_NAME/"$name"/g" "../workflows/$name.yml"
+
 mkdir $name
 cd $name
 
